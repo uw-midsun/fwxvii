596ddcdcf54c048e616cbfebbc16ef82de968b15
Lorenzo Good <foehammer@disroot.org>
2026-03-03 18:21:02 -0800

    Init repo

---
+++ nix/flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    inputs@{ ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
+      buildNixpkgs =
+        system:
+        import inputs.nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = buildNixpkgs system;
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              bazelisk
+              libtinfo
+            ];
+          };
+        }
+      );
+    };
+}
